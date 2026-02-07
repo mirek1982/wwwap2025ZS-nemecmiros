eb5e983ad278d4f2ec1d21f6dc3012de29bf77f4
mirek1982 <miroslav@nemec.li>
2026-02-07 19:40:00 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
 
 //<script>
     //vykreslíme si nějaký interface
-    
+
     
     
     function zaklad()  {
@@ -114,171 +114,50 @@
     }
     
     
-    let db;
+  let db;
     let dbVersion = 1;
     let dbReady = false;
     
+function initDb() {
+    let request = indexedDB.open('kartoteka', dbVersion);
     
+    request.onerror = function(e) {
+        console.error('Unable to open database.');
+    }
     
+    request.onsuccess = function(e) {
+    db = e.target.result;
+    console.log('db opened' + db);
+        
+  
+        
+        
+        let modul1= document.createElement("SCRIPT");
+
+        modul1.setAttribute('type','module');
+
+        let h1=document.createTextNode("import vypis from './js/vypis.js';vypis();");
+
+        modul1.appendChild(h1);
+
+        document.body.appendChild(modul1);
     
-    function initDb() {
-        let request = indexedDB.open('kartoteka', dbVersion);
-        
-        request.onerror = function(e) {
-            console.error('Unable to open database.');
-        }
-        
-        request.onsuccess = function(e) {
-            db = e.target.result;
-            console.log('db opened');
-            
-            
-            //  zjistim si pocatecni  hodnotu  id
-            let objectStore = db.transaction("cachedForms").objectStore("cachedForms");
-            objectStore.openCursor().onsuccess = event => {
-                let cursor = event.target.result;
-                
-                if(cursor)  {
-                    
-                    const ckey = cursor.key;
-                    
-                    
-                    
-                    
-                    
-                    let osoba = cursor.value;
-                    
-                    //export dat z databaze
-                    let textovadata = "{";
-                    let keys = Object.keys(osoba);
-                    let pocethodnot = keys.length;
-                    
-                    
-                    for (let i = 0; i < pocethodnot; i++)  {
-                        
-                        
-                        
-                        if (Object.keys(osoba)[i] == "data"){
-                            
-                            textovadata += '"' + Object.keys(osoba)[i]  + '":"' + Object.values(osoba)[i] + '"';
-                        }  else  {   textovadata += '"' + Object.keys(osoba)[i]  + '":"' + Object.values(osoba)[i] + '"';}
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        if (i <= pocethodnot-2) { textovadata += ',' ;}
-                    }
-                    
-                    textovadata += '}\n';
-                    // konec exportu
-                    
-                    
-                    
-                    
-                    let para = document.createElement("p");
-                    
-                    let br = document.createElement("BR");
-                    let hr = document.createElement("HR");
-                    para.setAttribute("id", "odstavec"+ckey);
-                    para.style.zIndex = "1";
-                    
-                    
-                    
-                    
-                    if(!osoba.jmeno)  {
-                        
-                        para.innerHTML="noname";
-                        
-                    } else {
-                        para.innerHTML=osoba.jmeno;
-                        
-                    }
-                    
-                    document.body.appendChild(para);
-                    let fot = document.createElement("IMG");
-                    let odata =  'data:image/jpeg;base64,' + osoba.data;
-                    
-                    fot.setAttribute("src", odata);
-                    fot.setAttribute("width", "300");
-                    fot.setAttribute("height", "200");
-                    fot.setAttribute("alt", osoba.jmeno);
-                    para.appendChild(fot);
-                    
-                    let smaz =  document.createElement("button");
-                    let popis1 = document.createTextNode("smaž");
-                    smaz.setAttribute('type','button');
-                    smaz.setAttribute('id','smaz'+ckey);
-                    smaz.setAttribute('value',ckey);
-                    
-                    
-                    
-                    let zalohuj = document.createElement('BUTTON');
-                    
-                    zalohuj.setAttribute('value',textovadata );
-                    zalohuj.setAttribute('type','button');
-                    zalohuj.setAttribute('id','button');
-                    
-                    let zalohujpopis = document.createTextNode('zálohuj');
-                    
-                    
-                    zalohuj.appendChild(zalohujpopis);
-                    
-                    zalohuj.addEventListener("click", downloadTextFile);
-                    
-                    smaz.appendChild(popis1);
-                    
-                    para.appendChild(smaz);
-                    para.appendChild(zalohuj);
-                    para.appendChild(br);
-                    
-                    document.getElementById('odstavec'+ckey).appendChild(hr);
-                    
-                    document.getElementById("smaz"+ckey).addEventListener('click', smazZaznam);
-                    
-                    
-                    
-                    
-                    
-                    
-                    cursor.continue();
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                }
-                
-                
-                
-                
-            }
-            
-            
-            
-            
-        }
-        
-        
-        
-        
-        
-        request.onupgradeneeded = function(e) {
-            let db = e.target.result;
-            let  item = db.createObjectStore('cachedForms', {keyPath:'id', autoIncrement: true});
-            
-            
-            
-            dbReady = true;
-        }
+  
         
     }
     
     
+    request.onupgradeneeded = function(e) {
+        let db = e.target.result;
+        let  item = db.createObjectStore('cachedForms', {keyPath:'id', autoIncrement: true});
+        
+        
+        
+        dbReady = true;
+    }
+}
+    
+
     
     
     // funkce smazat
@@ -475,4 +354,6 @@
     }
     
     
+    
+
 
--- js/vypis.js
+++ js/vypis.js
@@ -1,7 +1,118 @@
-//
-//  vypis.js
-//  
-//
-//  Created by Miroslav Němec on 06.02.2026.
-//
+export default function  vypis() {
+    
+  
+ 
+    
+    
+    let objectStore = db.transaction("cachedForms").objectStore("cachedForms");
+    objectStore.openCursor().onsuccess = event => {
+        let cursor = event.target.result;
+        
+        if(cursor)  {
+            
+            const ckey = cursor.key;
+            
+            let osoba = cursor.value;
+            
+            //export dat z databaze
+            let textovadata = "{";
+            let keys = Object.keys(osoba);
+            let pocethodnot = keys.length;
+            
+            
+            for (let i = 0; i < pocethodnot; i++)  {
+                
+                
+                
+                if (Object.keys(osoba)[i] == "data"){
+                    
+                    textovadata += '"' + Object.keys(osoba)[i]  + '":"' + Object.values(osoba)[i] + '"';
+                }  else  {   textovadata += '"' + Object.keys(osoba)[i]  + '":"' + Object.values(osoba)[i] + '"';}
+                
+                
+                
+                
+                
+                
+                
+                if (i <= pocethodnot-2) { textovadata += ',' ;}
+            }
+            
+            textovadata += '}\n';
+            // konec exportu
+            
+            
+            
+            
+            let para = document.createElement("p");
+            
+            let br = document.createElement("BR");
+            let hr = document.createElement("HR");
+            para.setAttribute("id", "odstavec"+ckey);
+            para.style.zIndex = "1";
+            
+            
+            
+            
+            if(!osoba.jmeno)  {
+                
+                para.innerHTML="noname";
+                
+            } else {
+                para.innerHTML=osoba.jmeno;
+                
+            }
+            
+            document.body.appendChild(para);
+            let fot = document.createElement("IMG");
+            let odata =  'data:image/jpeg;base64,' + osoba.data;
+            
+            fot.setAttribute("src", odata);
+            fot.setAttribute("width", "300");
+            fot.setAttribute("height", "200");
+            fot.setAttribute("alt", osoba.jmeno);
+            para.appendChild(fot);
+            
+            let smaz =  document.createElement("button");
+            let popis1 = document.createTextNode("smaž");
+            smaz.setAttribute('type','button');
+            smaz.setAttribute('id','smaz'+ckey);
+            smaz.setAttribute('value',ckey);
+            
+            
+            
+            let zalohuj = document.createElement('BUTTON');
+            
+            zalohuj.setAttribute('value',textovadata );
+            zalohuj.setAttribute('type','button');
+            zalohuj.setAttribute('id','button');
+            
+            let zalohujpopis = document.createTextNode('zálohuj');
+            
+            
+            zalohuj.appendChild(zalohujpopis);
+            
+            zalohuj.addEventListener("click", downloadTextFile);
+            
+            smaz.appendChild(popis1);
+            
+            para.appendChild(smaz);
+            para.appendChild(zalohuj);
+            para.appendChild(br);
+            
+            document.getElementById('odstavec'+ckey).appendChild(hr);
+            
+            document.getElementById("smaz"+ckey).addEventListener('click', smazZaznam);
+            
+            cursor.continue();
+            
+            
+            
+        }
+        
+    }
+    
+}
 
+    
+    
